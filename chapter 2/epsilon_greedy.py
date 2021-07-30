49a268036123bfbe4f1f375df8d8484c13eae9ef
Seongmin-Jo <76901622+seongmin-Jo@users.noreply.github.com>
2021-07-30 16:07:23 +0900

    Add files via upload

---
+++ chapter 2/epsilon_greedy.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class epsilon_greedy:
+    def __init__(self):
+        self.epsilon = 0.01
+        
+    def get_action(self):
+        if np.random.rand() < self.epsilon:
+            # 무작위 행동 반환
+            action = env.action_space.sample()
+        else:
+            # 최고의 행동 선택 
+            action = get_best_action(actions, rewards)
+        return action
